85b4ef0e725930f56d2e3ac73225a6baa79f005f
mrbean-0kill <mrbeancyber2024@gmail.com>
2024-07-06 22:10:56 +0530

    changes

--- sem4th_subjects.py
+++ sem4th_subjects.py
@@ -1,18 +1,18 @@
 num_sub = int(input("Enter number of subjects in 4th sem:"))
 subjects = []
 for i in range(num_sub):
-    sub = input("Enter subject i: ")
+    sub = input("Enter subjects: ")
     subjects.append(sub)
 
 print("Subjects in the 4th sem:\n")
 
 print(" Printing all the subjects in the 4th sem:")
-for i in range(subjects):
+for i in range(len(subjects)):
     print(subjects[i])
 
 print("2nd and 5th subjects in 4th sem ",subjects[1],"and",subjects[4])
-print("First 4 subjects of 4th sem:",subjects[:3])
-print("Last 4 subjects of 4th sem:",subjects[-1:-4])
+print("First 4 subjects of 4th sem:",subjects[:4])
+print("Last 4 subjects of 4th sem:",subjects[-4:])
 
 if "Python Programming Lab" in subjects:
     print("Yes we have")
@@ -24,7 +24,7 @@ new_sub = "XYZ"
 subjects.append(new_sub)
 print("Updates subjects after appending new subject:",subjects)
 new_sub1 = "ABC"
-subjects.insert(new_sub1,2)
+subjects.insert(2,new_sub1)
 print("After inserting at index 2 updates subjectss list is:",subjects)
 print("Demonstrating remove() and pop() function:")
 subjects.remove(new_sub)
